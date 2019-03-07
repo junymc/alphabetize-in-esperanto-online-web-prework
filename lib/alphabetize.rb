6599191c4fe3f0372214b0131dd4ed808a27b2d4
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-07 05:55:52 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,14 +1,3 @@
-# alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-#
-# def alphabetize(arr)
-#  arr.sort_by do |string|
-#     string.split(" ").map do |letter|
-#       alphabet.index(letter)
-#     end
-#   end
-# end
-
-
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
