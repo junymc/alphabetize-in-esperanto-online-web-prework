fd53c48295017095dc57a738216a9a0240353fd4
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-07 05:55:18 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,20 @@
-alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+#
+# def alphabetize(arr)
+#  arr.sort_by do |string|
+#     string.split(" ").map do |letter|
+#       alphabet.index(letter)
+#     end
+#   end
+# end
+
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
- arr.sort_by do |string|
-    string.split(" ").map do |letter|
-      alphabet.index(letter)
+  arr.sort_by do |string|
+    string.split("").map do |letter|
+    ALPHABET.index(letter)
     end
   end
 end
